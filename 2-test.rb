4f65fb9f750ad148873865b381fda95defcc9328
hasefumi23 <hasefumi23@gmail.com>
2020-02-10 15:37:35 +0900

    [from now] 2020/02/10 15:37:34
    diff --git a/2-test.rb b/2-test.rb
    index 525f585..30f254a 100644
    --- a/2-test.rb
    +++ b/2-test.rb
    @@ -151,27 +151,144 @@ class Assign < Struct.new(:name, :expression)
    
       def reduce(environment)
         if expression.reducible?
    -      [Assign.new(name, expresssion.reduce(environment)), environment]
    +      [Assign.new(name, expression.reduce(environment)), environment]
         else
           [DoNothing.new, environment.merge(name => expression)]
         end
       end
     end
    
    -class Machine < Struct.new(:expression, :environment)
    +class If < Struct.new(:condition, :consequence, :alternative)
    +  def to_s
    +    "if (#{condition}) { #{consequence} ) else { #{alternative} }"
    +  end
    +
    +  def inspect
    +    "<<#{self}>>"
    +  end
    +
    +  def reducible?
    +    true
    +  end
    +
    +  def reduce(environment)
    +    if condition.reducible?
    +      [If.new(condition.reduce(environment), consequence, alternative), environment]
    +    else
    +      case condition
    +      when Boolean.new(true)
    +        [consequence, environment]
    +      when Boolean.new(false)
    +        [alternative, environment]
    +      end
    +    end
    +  end
    +end
    +
    +class Machine < Struct.new(:statement, :environment)
       def step
    -    self.expression = expression.reduce(environment)
    +    self.statement, self.environment = statement.reduce(environment)
       end
    
       def run
    -    while expression.reducible?
    -      puts expression
    +    while statement.reducible?
    +      puts "#{statement}, #{environment}"
           step
         end
    -    puts expression
    +
    +    puts "#{statement}, #{environment}"
       end
     end
    
    +class Sequence < Struct.new(:first, :second)
    +  def to_s
    +    "#{first}; #{second}"
    +  end
    +
    +  def inspect
    +    "<<#{self}>>"
    +  end
    +
    +  def reducible?
    +    true
    +  end
    +
    +  def reduce(environment)
    +    case first
    +    when DoNothing.new
    +      [second, environment]
    +    else
    +      reduced_first, reduced_environment = first.reduce(environment)
    +      [Sequence.new(reduced_first, second), reduced_environment]
    +    end
    +  end
    +end
    +
    +class While < Struct.new(:condition, :body)
    +  def to_s
    +    "While (#{condition}) { #{body} }"
    +  end
    +
    +  def inspect
    +    "<<#{self}>>"
    +  end
    +
    +  def reducible?
    +    true
    +  end
    +
    +  def reduce(environment)
    +    [If.new(condition, Sequence.new(body, self), DoNothing.new), environment]
    +  end
    +end
    +
    +# 評価に失敗する例
    +Machine.new(
    +  Sequence.new(
    +    Assign.new(:x, Boolean.new(true)),
    +    Assign.new(:x, Add.new(Variable.new(:x), Number.new(1)))
    +  ),
    +  {}
    +)
    +
    +Machine.new(
    +  While.new(
    +    LessThan.new(Variable.new(:x), Number.new(5)),
    +    Assign.new(:x, Multiply.new(Variable.new(:x), Number.new(3)))
    +  ),
    +  x: Number.new(1)
    +).run
    +
    +Machine.new(
    +  Sequence.new(
    +    Assign.new(:x, Add.new(Number.new(1), Number.new(1))),
    +    Assign.new(:y, Add.new(Variable.new(:x), Number.new(3)))
    +  ),
    +  {}
    +).run
    +
    +Machine.new(
    +  If.new(
    +    Variable.new(:x),
    +    Assign.new(:y, Number.new(1)),
    +    Assign.new(:y, Number.new(2))
    +  ),
    +  x: Boolean.new(true)
    +).run
    +
    +Machine.new(
    +  If.new(
    +    Variable.new(:x),
    +    Assign.new(:y, Number.new(1)), DoNothing.new
    +  ),
    +  x: Boolean.new(false)
    +).run
    +
    +Machine.new(
    +  Assign.new(:x, Add.new(Variable.new(:x), Number.new(1))),
    +  x: Number.new(2)
    +).run
    +
     expression = Add.new(
       Multiply.new(Number.new(1), Number.new(2)),
       Multiply.new(Number.new(3), Number.new(4))

--- 2-test.rb
+++ 2-test.rb
@@ -151,26 +151,143 @@ class Assign < Struct.new(:name, :expression)
 
   def reduce(environment)
     if expression.reducible?
-      [Assign.new(name, expresssion.reduce(environment)), environment]
+      [Assign.new(name, expression.reduce(environment)), environment]
     else
       [DoNothing.new, environment.merge(name => expression)]
     end
   end
 end
 
-class Machine < Struct.new(:expression, :environment)
+class If < Struct.new(:condition, :consequence, :alternative)
+  def to_s
+    "if (#{condition}) { #{consequence} ) else { #{alternative} }"
+  end
+
+  def inspect
+    "<<#{self}>>"
+  end
+
+  def reducible?
+    true
+  end
+
+  def reduce(environment)
+    if condition.reducible?
+      [If.new(condition.reduce(environment), consequence, alternative), environment]
+    else
+      case condition
+      when Boolean.new(true)
+        [consequence, environment]
+      when Boolean.new(false)
+        [alternative, environment]
+      end
+    end
+  end
+end
+
+class Machine < Struct.new(:statement, :environment)
   def step
-    self.expression = expression.reduce(environment)
+    self.statement, self.environment = statement.reduce(environment)
   end
 
   def run
-    while expression.reducible?
-      puts expression
+    while statement.reducible?
+      puts "#{statement}, #{environment}"
       step
     end
-    puts expression
+
+    puts "#{statement}, #{environment}"
   end
 end
+
+class Sequence < Struct.new(:first, :second)
+  def to_s
+    "#{first}; #{second}"
+  end
+
+  def inspect
+    "<<#{self}>>"
+  end
+
+  def reducible?
+    true
+  end
+
+  def reduce(environment)
+    case first
+    when DoNothing.new
+      [second, environment]
+    else
+      reduced_first, reduced_environment = first.reduce(environment)
+      [Sequence.new(reduced_first, second), reduced_environment]
+    end
+  end
+end
+
+class While < Struct.new(:condition, :body)
+  def to_s
+    "While (#{condition}) { #{body} }"
+  end
+
+  def inspect
+    "<<#{self}>>"
+  end
+
+  def reducible?
+    true
+  end
+
+  def reduce(environment)
+    [If.new(condition, Sequence.new(body, self), DoNothing.new), environment]
+  end
+end
+
+# 評価に失敗する例
+Machine.new(
+  Sequence.new(
+    Assign.new(:x, Boolean.new(true)),
+    Assign.new(:x, Add.new(Variable.new(:x), Number.new(1)))
+  ),
+  {}
+)
+
+Machine.new(
+  While.new(
+    LessThan.new(Variable.new(:x), Number.new(5)),
+    Assign.new(:x, Multiply.new(Variable.new(:x), Number.new(3)))
+  ),
+  x: Number.new(1)
+).run
+
+Machine.new(
+  Sequence.new(
+    Assign.new(:x, Add.new(Number.new(1), Number.new(1))),
+    Assign.new(:y, Add.new(Variable.new(:x), Number.new(3)))
+  ),
+  {}
+).run
+
+Machine.new(
+  If.new(
+    Variable.new(:x),
+    Assign.new(:y, Number.new(1)),
+    Assign.new(:y, Number.new(2))
+  ),
+  x: Boolean.new(true)
+).run
+
+Machine.new(
+  If.new(
+    Variable.new(:x),
+    Assign.new(:y, Number.new(1)), DoNothing.new
+  ),
+  x: Boolean.new(false)
+).run
+
+Machine.new(
+  Assign.new(:x, Add.new(Variable.new(:x), Number.new(1))),
+  x: Number.new(2)
+).run
 
 expression = Add.new(
   Multiply.new(Number.new(1), Number.new(2)),
